25b4c0059f29f15906cebff160141db78a8a5987
sandip chakraborty <unicornsc77@gmail.com>
2025-06-20 09:55:15 +0530

    we add someccode in package.json folder

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Todo-List/',
+  base: '/ToDo-List',
   plugins: [
     tailwindcss(),
     react()
