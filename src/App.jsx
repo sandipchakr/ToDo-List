50b8bfd3c17a648df9ff220fdb96a3a7d13148a0
sandip chakraborty <unicornsc77@gmail.com>
2025-06-21 20:05:35 +0530

    we remove the dist folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import {TodoProvider} from './contexts'
-import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
 
